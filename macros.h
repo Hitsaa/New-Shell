c9e73a5d15e176b00b31646e212aaf91ae2d6fd8
Hitendra Singh Shekhawat <33285810+Hitsaa@users.noreply.github.com>
2019-07-28 16:00:03 +0530

    Update macros.h

--- macros.h
+++ macros.h
@@ -2,6 +2,30 @@
 
 #ifndef MACROS_H
 #define MACROS_H
+/***************************Header Files Used*****************************/
+void clear_variables();
+void fileprocess ();
+void filewrite();
+void bang_execute();
+void environmment();
+void set_environment_variables();
+void change_directory();
+void parent_directory();
+void echo_calling(char *echo_val);
+void history_execute_with_constants();
+static char* skipwhite(char* s);
+void tokenise_commands(char *com_exec);
+void tokenise_redirect_input_output(char *cmd_exec);
+void tokenise_redirect_input(char *cmd_exec);
+void tokenise_redirect_output(char *cmd_exec);
+char* skipcomma(char* str);
+static int split(char *cmd_exec, int, int, int);
+void with_pipe_execute();
+static int command(int, int, int, char *cmd_exec);
+void prompt();
+void sigintHandler(int sig_num);
+
+/*************************************************************************/
 
 
 #endif
